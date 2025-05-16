660fccd954b2d2ebd01ec6fc8a4403b69fb1e89e
Abhishek Jha <121544518+AbhishekHacks@users.noreply.github.com>
2025-05-17 01:55:29 +0530

    Added questions.js

---
+++ QUIZ GAME WEB APP/questions.js
@@ -0,0 +1,52 @@
+const quizQuestions = [
+  {
+    question: "What does HTML stand for?",
+    options: [
+      "Hyperlinks and Text Markup Language",
+      "Home Tool Markup Language",
+      "Hyper Text Markup Language",
+      "Hyper Tool Mark Language"
+    ],
+    correctAnswer: 2
+  },
+  {
+    question: "Which language is used for styling web pages?",
+    options: [
+      "HTML",
+      "JQuery",
+      "CSS",
+      "XML"
+    ],
+    correctAnswer: 2
+  },
+  {
+    question: "Which is not a JavaScript data type?",
+    options: [
+      "Undefined",
+      "Number",
+      "Float",
+      "Boolean"
+    ],
+    correctAnswer: 2
+  },
+  {
+    question: "Which symbol is used for comments in JavaScript?",
+    options: [
+      "//",
+      "<!-- -->",
+      "#",
+      "/* */"
+    ],
+    correctAnswer: 0
+  },
+  {
+    question: "What is the correct syntax to link an external CSS file?",
+    options: [
+      "<link rel='stylesheet' href='style.css'>",
+      "<style src='style.css'>",
+      "<script src='style.css'>",
+      "<css>style.css</css>"
+    ],
+    correctAnswer: 0
+  }
+];
